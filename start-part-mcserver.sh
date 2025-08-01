25d2fd7bae447af69a6724bbb576a3b684f400b5
wujinjun-MC <qq1602647782@163.com>
2025-08-01 23:47:50 +0800

    增加一种JVM参数(默认禁用)

--- start-part-mcserver.sh
+++ start-part-mcserver.sh
@@ -25,6 +25,8 @@ exit_actions()
 # trap exit_actions INT
 
 jvm="-server -Xmx${maxmem} -Xms${minmem} -Xnoclassgc -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+PerfDisableSharedMem -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -XX:MaxInlineLevel=16 -XX:MaxGCPauseMillis=200 -XX:+UseCompressedOops -XX:+UseLargePages -XX:+ExplicitGCInvokesConcurrent -XX:FreqInlineSize=325 -XX:MaxInlineSize=35 -XX:InlineSmallCode=2000 -XX:MaxRecursiveInlineLevel=1 -XX:MaxTenuringThreshold=1 -XX:SurvivorRatio=32 -XX:-DontCompileHugeMethods -XX:-CompactStrings -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
+# jvm="-server -Xmx${maxmem} -Xms${minmem} -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
+
 
 while true
 do
