210a345f5d574cac4a63b34b9233a477e8c5f090
wujinjun-MC <89054683+wujinjun-MC@users.noreply.github.com>
2026-08-17 20:04:12 +0800

    `scripts/diskusage.sh`: Fix bash shebang path and remove invalid `^M` character

--- scripts/diskusage.sh
+++ scripts/diskusage.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/bash
-export PATH=$PATH:/home/container/bin
+#!/bin/bash
+
+export PATH="$PATH":/home/container/bin
 tmux new -s disk-usage "ncdu -r --enable-refresh --apparent-size --enable-natsort -e --show-percent --show-graph --show-mtime --show-itemcount"'; sleep 1'
